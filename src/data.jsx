64a16f34535aaf76e09837bd5c523cc1d6c8b7e1
hamiz <reshma9298h@gmail.com>
2024-04-06 13:59:25 +0530

    ready

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,6 @@
 import { BsPieChartFill, BsFillDiagram3Fill } from "react-icons/bs";
 import { BsFillFileEarmarkCodeFill } from "react-icons/bs";
-import { FaFileInvoice } from "react-icons/bs";
+import { FaFileInvoice } from "react-icons/fa";
 import { MdOutlineDeveloperMode } from "react-icons/md";
 
 /*static data*/
