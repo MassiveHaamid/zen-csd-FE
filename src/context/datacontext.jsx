77b8f09abf49e35bfc92694de3dfd8c603c23fbf
hamiz <reshma9298h@gmail.com>
2024-04-06 06:03:48 +0530

    ready

--- src/context/datacontext.jsx
+++ src/context/datacontext.jsx
@@ -25,6 +25,7 @@ export const DataProvider = ({ children }) => {
   //for pages
   const [day, setDay] = useState(0);
   const [data, setData] = useState(roadMapData[0]);
+  const [flag, setFlag] = useState(true);
   const [toggle, setToggle] = useState(false);
   const [frontEndCode, setFrontEndCode] = useState("");
   const [frontEndURL, setFrontEndURL] = useState("");
@@ -336,6 +337,8 @@ export const DataProvider = ({ children }) => {
         setDay,
         data,
         setData,
+        flag,
+        setFlag,
         frontEndCode,
         setFrontEndCode,
         frontEndURL,
